28927f3c05dd4bac129fe0bbe3b19f183978979a
anupzpt <prajapatianu433@gmail.com>
2022-11-28 23:11:06 +0545

    bootstrap installation in laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.app')
+@section('content')
+<h1>Welcome</h1>
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
+@endsection
